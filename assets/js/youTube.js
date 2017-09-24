8cd3c5af3faed3deaa870badd5e3f8454bf26744
Seeanneb <Seeanneb@gmail.com>
2017-09-23 21:31:01 -0700

    working Youtube API

--- assets/js/youTube.js
+++ assets/js/youTube.js
@@ -1,154 +1,33 @@
-/***** START BOILERPLATE CODE: Load client library, authorize user. *****/
+$(document).ready(function(){
+  $(function() {
+    $("form").submit(function() { return false; }); // prevents page from reload on "enter press"
+});
+  var q ;
+  var buildUrl = "https://www.googleapis.com/youtube/v3/search" + q
+//establish variables to build AJAX call
 
-  // Global variables for GoogleAuth object, auth status.
-  var GoogleAuth;
+$("#searchButton").on("click", function(){
+    q = $("#search").val()
+  console.log(q)
+  $("#vidTitle").html(q)
+ 
+// user input that stores the search variable
 
-  /**
-   * Load the API's client and auth2 modules.
-   * Call the initClient function after the modules load.
-   */
-  function handleClientLoad() {
-    gapi.load('client:auth2', initClient);
-  }
+  q = $("#search").val()
+  $("#vidTitle").html(q)
 
-  function initClient() {
-    // Initialize the gapi.client object, which app uses to make API requests.
-    // Get API key and client ID from API Console.
-    // 'scope' field specifies space-delimited list of access scopes
-
-    gapi.client.init({
-        'clientId': '474575811033-srm92po31lsearpv0shp67qsjf8f3rt2.apps.googleusercontent.com',
-        'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'],
-        'scope': 'https://www.googleapis.com/auth/youtube.force-ssl https://www.googleapis.com/auth/youtubepartner'
-    }).then(function () {
-      GoogleAuth = gapi.auth2.getAuthInstance();
-
-      // Listen for sign-in state changes.
-      GoogleAuth.isSignedIn.listen(updateSigninStatus);
-
-      // Handle initial sign-in state. (Determine if user is already signed in.)
-      setSigninStatus();
-
-      // Call handleAuthClick function when user clicks on "Authorize" button.
-      $('#execute-request-button').click(function() {
-        handleAuthClick(event);
-      }); 
+  $.ajax({
+    method: "GET",
+    url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q='+ q +'&regionCode=us&type=video&videoCategoryId=10&videoDefinition=any&videoEmbeddable=true&videoSyndicated=true&key=AIzaSyCsoCs7rQgLp_KQrlZP8oe6UHvo6taoHbY'
+  })
+    .done(function(response){
+      
+     var results = response.items;
+     var youtubeSearch = 'https://www.youtube.com/embed/' + results[0].id.videoId;
+     
+     $("#vidHere").attr("src", youtubeSearch + "?autoplay=1")
     });
-  }
+    
+  });
 
-  function handleAuthClick(event) {
-    // Sign user in after click on auth button.
-    GoogleAuth.signIn();
-  }
-
-  function setSigninStatus() {
-    var user = GoogleAuth.currentUser.get();
-    isAuthorized = user.hasGrantedScopes('https://www.googleapis.com/auth/youtube.force-ssl https://www.googleapis.com/auth/youtubepartner');
-    // Toggle button text and displayed statement based on current auth status.
-    if (isAuthorized) {
-      defineRequest();
-    }
-  }
-
-  function updateSigninStatus(isSignedIn) {
-    setSigninStatus();
-  }
-
-  function createResource(properties) {
-    var resource = {};
-    var normalizedProps = properties;
-    for (var p in properties) {
-      var value = properties[p];
-      if (p && p.substr(-2, 2) == '[]') {
-        var adjustedName = p.replace('[]', '');
-        if (value) {
-          normalizedProps[adjustedName] = value.split(',');
-        }
-        delete normalizedProps[p];
-      }
-    }
-    for (var p in normalizedProps) {
-      // Leave properties that don't have values out of inserted resource.
-      if (normalizedProps.hasOwnProperty(p) && normalizedProps[p]) {
-        var propArray = p.split('.');
-        var ref = resource;
-        for (var pa = 0; pa < propArray.length; pa++) {
-          var key = propArray[pa];
-          if (pa == propArray.length - 1) {
-            ref[key] = normalizedProps[p];
-          } else {
-            ref = ref[key] = ref[key] || {};
-          }
-        }
-      };
-    }
-    return resource;
-  }
-
-  function removeEmptyParams(params) {
-    for (var p in params) {
-      if (!params[p] || params[p] == 'undefined') {
-        delete params[p];
-      }
-    }
-    return params;
-  }
-
-  function executeRequest(request) {
-    request.execute(function(response) {
-      console.log(response);
-      outputResults(response);
-    });
-  }
-
-  function buildApiRequest(requestMethod, path, params, properties) {
-    params = removeEmptyParams(params);
-    var request;
-    if (properties) {
-      var resource = createResource(properties);
-      request = gapi.client.request({
-          'body': resource,
-          'method': requestMethod,
-          'path': path,
-          'params': params
-      });
-    } else {
-      request = gapi.client.request({
-          'method': requestMethod,
-          'path': path,
-          'params': params
-      });
-    }
-    executeRequest(request);
-  }
-
-  /***** END BOILERPLATE CODE *****/
-  /***** PROJECT CODE *****/
-  function outputResults(response) {
-    var items = response.items;
-    for (var i = 0; i < items.length; i++) {
-      outputItem(items[i]);
-    }
-  }
-
-  function outputItem(item) {
-    // Build Link
-   var link = $('<a>').attr('href', 'https://www.youtube.com/watch?v=' + item.id.videoId)
-                      .html(item.snippet.title);
-  // Create container and append to Results Div
-   $('<div>').append(link)
-              .appendTo('#results');
-  }
-  
-  function defineRequest() {
-    // See full sample for buildApiRequest() code, which is not 
-// specific to a particular youtube or youtube method.
-
-buildApiRequest('GET',
-                '/youtube/v3/search',
-                {'maxResults': '5',
-                 'part': 'snippet',
-                 'q': 'Deez nuts',
-                 'type': 'video'});
-
-  }
+});
