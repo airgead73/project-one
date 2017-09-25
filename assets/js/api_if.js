07e51dad0e13dc9ae55a786d9a13fa00fe014656
alan davison <alan.b.davison@gmail.com>
2017-09-25 16:21:28 -0700

    apiObj queries all returning data from musixmatch api. Have keyworkSearch, songSearch, artistSearch, lyricsSearch and countrySearch.

--- assets/js/api_if.js
+++ assets/js/api_if.js
@@ -5,10 +5,7 @@
  *  - design note: Possible convenience feature - should really not need these for anything
  */
 var gblResponse = {};
-var gblApiName = '';
-var gblBaseUrl = '';
-var gblSearchType = '';
-var gblCountry = '';
+var test = null;
 
 /**
  * apiObj - this is the main wrapper object to access all api related stuff
@@ -76,6 +73,21 @@ var apiObj = {
 		this.callAjax(queryString);
 	},
 
+	countrySearch: function (searchString, apiName, searchParam) {
+		console.log("apiObj.countrySearch fn: searchString: ", searchString);
+		apiName = (typeof apiName !== 'undefined') ?  apiName : 'musixmatch';		// default to musixMatch if no name is given
+		searchParam = (typeof searchParam !== 'undefined') ?  searchParam : 'page=1&page_size=3&';	// default to this if no param is given\
+
+		var search = searchString;
+		var baseUrl = getBaseUrl(apiName);
+		var searchType = getSearchType('countrySearch');
+		var key = getKey(apiName);
+		queryString = baseUrl + searchType + 'country=' + searchString + '&' + searchParam + key;
+		console.log("apiObj.lyricsSearch: queryString: ", queryString);
+
+		this.callAjax(queryString);
+	},
+
 	videoSearch: function (keyword) {
 		//return apiObj ();
 	},
@@ -121,43 +133,79 @@ var apiObj = {
 			console.log("apiObj: processResponse: response.message.body", response.message.body);
 		};
 
+		if (response.message.body.hasOwnProperty('track_list')) {
+			console.log ("apiObj: processResponse.message.body.track_list: ", response.message.body.track_list);
+			console.log ("apiObj: processResponse.message.body.track_list[0]: ", response.message.body.track_list[0]);
+			console.log ("apiObj: processResponse.message.body.track_list[0].track: ", response.message.body.track_list[0].track);
+			console.log ("apiObj: processResponse.message.body.track_list[0].track.track_id: ", response.message.body.track_list[0].track.track_id);
 
-		//console.log ("apiObj: processResponse.message.body: ", response.message.body);
+			this.track_id = response.message.body.track_list[0].track.track_id;
+			this.track_name	= response.message.body.track_list[0].track.track_name;
+			this.lyrics_id = response.message.body.track_list[0].track.lyrics_id;
+			this.album_id = response.message.body.track_list[0].track.album_id;
+			this.album_name = response.message.body.track_list[0].track.album_name;
+			this.album_art = response.message.body.track_list[0].track.album_coverart_100x100;
+		};
 
-		//console.log ("apiObj: processResponse.message.body.lyrics.lyrics.body: ", response.message.body.lyrics.lyrics_body);
+		if (response.message.body.hasOwnProperty('artist_list')) {
+			console.log ("apiObj: processResponse.message.body.artist_list: ", response.message.body.artist_list);
+			console.log ("apiObj: processResponse.message.body.artist_list[0]: ", response.message.body.artist_list[0]);
+			console.log ("apiObj: processResponse.message.body.artist_list[0].artist: ", response.message.body.artist_list[0].artist);
+			console.log ("apiObj: processResponse.message.body.artist_list[0].artist.artist_id: ", response.message.body.artist_list[0].artist.artist_id);
 
-		var myObject = {level1: {level2: {level3: {level4: {level5: 'track_list'}}}}};
-		console.log ("Object.get: ", Object.get(myObject, 'level1.level2.level3.level4.level5'));
+			this.artist_id = response.message.body.artist_list[0].artist.artist_id;
+			this.artist_name	= response.message.body.artist_list[0].artist.artist_name;
+		};	
 
-		if(typeof response.message.body.track_list != undefined) {
-			console.log ("apiObj: processResponse.message.body.track_list: ", response.message.body.track_list);			
-		}
-		//console.log ("apiObj: processResponse.message.body.track_list[0].track.track_name: ", response.message.body.track_list[0].track.track_name);
+		if (response.message.body.hasOwnProperty('lyrics')) {
+			console.log ("apiObj: processResponse.message.body.lyrics: ", response.message.body.lyrics);
 
+			this.lyrics_body = response.message.body.lyrics.lyrics_body;
 
+			this.showApiData();
 
-		//console.log("apiObj.processResponse: ", myResponse[100]);
+		};
+
+		console.log("Object.keys: ", Object.keys(response)); // shows single key "message" in red
 
 	},
 
 
 
 	showApiData: function () {
-		console.log("showApiData function called");
+		//console.log("showApiData function called");
 
-		console.log("apiData: artist_id: ", this.apiData.artist_id);		
-		console.log("apiData: artist_name: ", this.apiData.artist_name);		
-		console.log("apiData: album_id: ", this.apiData.album_id);		
-		console.log("apiData: album_name: ", this.apiData.album_name);		
-		console.log("apiData: album_art: ", this.apiData.album_art);		
-		console.log("apiData: track_id: ", this.apiData.track_id);		
-		console.log("apiData: track_name: ", this.apiData.track_name);		
-		console.log("apiData: lyrics_id: ", this.apiData.lyrics_id);		
-		console.log("apiData: lyrics_body: ", this.apiData.lyrics_body);	
+		console.log("\n");
+		console.log("======================================");
+		console.log("          API DATA");
+		console.log("======================================");
+		console.log("\t" + "artist_id: ", this.artist_id);		
+		console.log("\t" + "artist_name: ", this.artist_name);		
+		console.log("\t" + "album_id: ", this.album_id);		
+		console.log("\t" + "album_name: ", this.album_name);		
+		console.log("\t" + "album_art: ", this.album_art);		
+		console.log("\t" + "track_id: ", this.track_id);		
+		console.log("\t" + "track_name: ", this.track_name);		
+		console.log("\t" + "lyrics_id: ", this.lyrics_id);		
+		console.log("\t" + "lyrics_body: ", this.lyrics_body);	
+		console.log('\n');
+	
+	},
 
-		//console.log("placeholder: ", this.);		
-		//console.log("placeholder: ", this.);		
-		//console.log("placeholder: ", this.);		
+	clearApiData: function () {
+		//console.log("clearApiData function called");
+
+		artist_id = null;		
+		artist_name = "";	
+		album_id = null;	
+		album_name = "";		
+		album_art =	"";	
+		track_id = null;	
+		track_name = "";		
+		lyrics_id =	null;
+		lyrics_body = "";
+		album_year: "";	
+	
 	},
 
 	firstLevelFunction: function () {
@@ -172,7 +220,7 @@ var apiObj = {
 		console.log("In helperFn: ");
 	},
 
-	apiData: {
+	//apiData: {
 
 		artist_id: 0,
 		artist_name: "",
@@ -180,12 +228,12 @@ var apiObj = {
 		album_name: "",
 		album_art: "",
 		track_id: 0,
-		track_name: "lawdkjflaksdfjla",
+		track_name: "",
 		lyrics_id: 12345,
 		lyrics_body: "Oh baby baby",
 		album_year: "1955",
 
-	},
+	//},
 
 };	// apiObject
 
@@ -205,8 +253,6 @@ Object.get = function(object, property) {
 function fooDone (response) {
 	console.log ("fooDone: ", JSON.parse(response));
 };
-
-
 
 /**
  * Plain Vanilla Functions - you can call these on their own to return a result string 
@@ -270,6 +316,22 @@ function lyricsSearch (searchString, apiName, searchParam) {
 	callAjax(queryString);
 }
 
+function countrySearch (searchString, apiName, searchParam) {
+		console.log("apiObj.countrySearch fn: searchString: ", searchString);
+		apiName = (typeof apiName !== 'undefined') ?  apiName : 'musixmatch';		// default to musixMatch if no name is given
+		searchParam = (typeof searchParam !== 'undefined') ?  searchParam : 'page=1&page_size=3&';	// default to this if no param is given\
+
+		var search = searchString;
+		var baseUrl = getBaseUrl(apiName);
+		var searchType = getSearchType('countrySearch');
+		var key = getKey(apiName);
+		queryString = baseUrl + searchType + 'track_id=' + searchString + '&' + searchParam + key;
+		console.log("apiObj.lyricsSearch: queryString: ", queryString);
+
+		this.callAjax(queryString);
+}
+
+
 /**
  * Helper Functions
  *  - used by other functions
@@ -281,7 +343,10 @@ function lyricsSearch (searchString, apiName, searchParam) {
 function getBaseUrl (apiName) {
 	//console.log("getBaseUrl: apiName", apiName);
 
-	var proxy = 'https://cors.io/?'
+	//var proxy = 'https://cors.io/?'
+	//var proxy = 'http://cors-proxy.htmldriven.com/?url='
+	//var proxy = 'https://crossorigin.me/'
+	var proxy = 'https://cors-anywhere.herokuapp.com/'
 	var baseUrl = '';
 
 	switch (apiName) {
@@ -296,7 +361,7 @@ function getBaseUrl (apiName) {
 			break;		
 		case 'bandsintown':
 			baseUrl = 'https://rest.bandsintown.com/';
-			break;		
+			break;	
 		case 'eventful':
 			baseUrl = 'https://eventful.com/';
 			break;		
@@ -349,7 +414,7 @@ function getSearchType (searchType, secondParam) {
 
 	switch (searchType) {
 		case 'countrySearch': 			// musixMatch only for now - chart.artists.get?page=1&page_size=3&country=it& 'apikey=4dd81b4d24fc4b88c41b0e8638cc97aa'
-			searchTypeString = '';
+			searchTypeString = 'chart.artists.get?';
 			break;
 		case 'trackSearch': 			// fma - trackSearch?q=deerhoof&limit=10' 
 			searchTypeString = 'track.search?';
@@ -363,7 +428,7 @@ function getSearchType (searchType, secondParam) {
 		case 'keywordSearch': 						// musixmatch track.search?q=sun&page_size=10&page=1&s_track_rating=desc&', 'apikey=4dd81b4d24fc4b88c41b0e8638cc97aa'
 			searchTypeString = 'track.search?';	// track.search?q=sun&page_size=10&page=1&s_track_rating=desc&', 'apikey=4dd81b4d24fc4b88c41b0e8638cc97aa'';
 			break;		
-		case '4': 						// musixmatch
+		case 'test': 						// musixmatch
 			searchTypeString = '';
 			break;		
 		default:
@@ -382,7 +447,7 @@ function callAjax (queryString) {
     console.log("callAjax function: queryString: ", queryString);
 
 	$.ajax({
-		  //dataType: 'jsonp',
+		  dataType: 'json',		// otherwise you get a very long char array!
           url: queryString,
           method: "GET"    
         }).done(function(response) {
@@ -400,6 +465,7 @@ function callAjax (queryString) {
     	apiObj.songSearch("rocket man");
     	apiObj.artistSearch("Van Morrison");
     	apiObj.lyricsSearch(15953433);
+    	apiObj.countrySearch("it");
     	apiObj.showApiData();
 
     });
