d0fd94def66864fa67837156b9ec9b77db779ff7
= <aguirremra@gmail.com>
2017-09-25 18:20:19 -0700

    added new app.js file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,13 @@
+var openAccess = $("#open-user-forms");
+var closeAccess = $("#close-user-forms");
+var profile = $("#profile");
+var userForms = $("#user-forms");
+var results = $("#results-container");
+var searchOpen = $("#searchButton");
+var searchClose = $("#close-search-container");
+var detail = $("#detail-container");
+var detailClose = $("#close-detail-container");
+
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyBND2-Ea8JLnajfN-fKzjSv7NZbcL25EGY",
@@ -11,7 +21,7 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
-
+//firebase variables
 var name = "";
 var username = "";
 
@@ -19,25 +29,37 @@ var username = "";
 function getCurrentUser(u, cb) {
     //gets the logged in user and returns tracks
     database.ref("users").orderByChild('username').equalTo(u).on("child_added", function(snapshot){
-        var userId = snapshot.key;
-        sessionStorage.setItem('userId', userId);
-
         var sv = snapshot.val();
-        cb(sv);           
+        var userId = snapshot.key;       
+        sessionStorage.setItem('userId', userId);
+        changeUserStatus();
+        var newStatus = $(openAccess).attr("data-status");
+        console.log("Log in was clicked. User status is now " + newStatus +"."); 
+        cb(sv);
     });
 }
+
+function changeUserStatus() {
+    var userStatus = $(openAccess).attr("data-status");
+    $(openAccess).attr("data-status", "logged");
+    $(openAccess).attr("value", "view your favorites");
+}
+
 //sign in to application
 $("#login").on("click", function(){
     event.preventDefault();
-    loginuser = $("#un").val().trim();    
-
-    getCurrentUser(loginuser, function(sv) {
-        for(var key in sv.tracks) {
-           updateFavorites(sv.tracks[key]);
-            // console.log(sv.tracks[key]);
-        }            
-    });
-
+    loginuser = $("#un").val().trim();
+    console.log("you entered loginuser name: " + loginuser);
+    if(loginuser === ""){
+        console.log("Enter a username to continue");
+    }else{
+        getCurrentUser(loginuser, function(sv) {
+            for(var key in sv.tracks) {
+               writeFavorites(sv.tracks[key]);
+                console.log(sv.tracks[key]);
+            }            
+        });
+    }
 });
 
 $("#signup").on("click", function(){
@@ -60,7 +82,9 @@ $("#signup").on("click", function(){
 $('#fav-save').click(function() {
     var userId = sessionStorage.getItem('userId');
     if(userId === null){
-        alert("You need to log in to save a track");
+        //alert("You need to log in to save a track");
+        $("#loginerror").append("<p>You need to log in to save favorites</p>");
+        console.log("you need to login");
     }else{
         var currentTracks = [];
         database.ref('users').child(userId).once('value').then(function(snapshot) {
@@ -70,21 +94,107 @@ $('#fav-save').click(function() {
             }
             // console.log(snapshot.val());
             //getting object into an array
-            // console.log(currentTracks);            
-            $("input:checkbox:checked", "#results").each(function() {
+            // console.log("current tracks: " + currentTracks);            
+            $("input:checkbox:checked", "#results-items").each(function() {
                 var track = $(this).data("for");
-                // console.log(track);
+                // console.log("checked track: "  + track);
                 if(currentTracks.indexOf(track) === -1){
                     database.ref("users/"+ userId +"/tracks").push(track);
                     currentTracks.push(track);
-                    updateFavorites(track);
+                    writeFavorites(track);
                 }
             });
         });
     }
 });
 //add favorites to table
-function updateFavorites(tracks){
+function writeFavorites(tracks){
     $("#my-favs")
     .append("<tr><td>"+tracks+"</td><td>Track Name</td><td>Artist</td><td>Album</td><td>Year</td></tr>");
 }
+
+//code to write search results to the table, will be a function 
+$("#searchButton").click(function(){
+    $("#results-items")
+        .append("<tr><td>Song3</td><td>Track Name</td><td>Artist</td><td>Album</td><td>Year</td><td><input class='fav' data-for='Song3' type='checkbox'></td></tr>");
+});
+
+function open(elem) {
+    $(elem).addClass("js-active");
+}
+
+function close(elem) {
+    $(elem).removeClass("js-active");
+}
+
+
+// When person clicks button at top right of page ...
+$(openAccess).on("click", function(){
+    var userStatus = $(openAccess).attr("data-status");
+    
+    // if person not logged in yet, signup/login form pops up
+    if (userStatus === "notLogged") {
+        console.log("opening log in screen");
+        open(userForms);
+        
+    // if person has logged in, favorites will appear instead of login
+    } else if (userStatus === "logged") {
+        console.log("favorites will open instead");
+        $(profile).addClass("js-active");
+        $("#main-logo").removeClass("col-6").addClass("col-2");
+        $("intro").removeClass("col-6").addClass("col-10");
+    }   
+});
+
+// closes signup/login box
+$(closeAccess).on("click", function(){
+        console.log("closing log in screen");
+        close(userForms);       
+    
+});
+
+// Opens search
+//Would need to be combined with other functions that are tied to #searchButton on app.js
+
+$(searchOpen).on("click", function(event){
+    event.preventDefault();
+    open(results);
+});
+
+
+// closes search field
+$(searchClose).on("click", function(event){
+    event.preventDefault();
+    close(results);
+});
+
+// open details from search result row
+// $("#results-items tr").on("click", function(){
+//  console.log("Table row clicked");   
+//  open(detail);   
+// });
+
+$(detailClose).on("click", function(){
+    console.log("Closing detail");
+    close(detail);  
+});
+
+
+$(document).ready(function(){
+    var userId = sessionStorage.getItem('userId');
+    if(userId === null){
+        console.log("user is not logged in");
+    }else{
+        changeUserStatus();
+        var newStatus = $(openAccess).attr("data-status");
+        console.log("User status is " + newStatus +".");
+        var currentTracks = [];
+        database.ref('users').child(userId).once('value').then(function(snapshot) {
+            var sv = snapshot.val();
+            for(var key in sv.tracks) {
+               writeFavorites(sv.tracks[key]);
+               console.log(sv.tracks[key]);
+            }        
+        });
+    }
+});
