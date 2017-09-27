709632bc0d9dbc58f6d31d5dc2ab905d32549820
airgead73 <catullus1973@gmail.com>
2017-09-27 01:13:17 -0700

    added my favs id to main selector line 11, so video will play from favorites.

--- assets/js/youTube.js
+++ assets/js/youTube.js
@@ -1,12 +1,14 @@
-$(document).ready(function(){
-  $(function() {
-    $("form").submit(function() { return false; }); // prevents page from reload on "enter press"
-});
-  var q ;
-  var buildUrl = "https://www.googleapis.com/youtube/v3/search" + q;
-//establish variables to build AJAX call
+$(document).ready(function () {
+	$(function () {
+		$("form").submit(function () {
+			return false;
+		}); // prevents page from reload on "enter press"
+	});
+	var q;
+	var buildUrl = "https://www.googleapis.com/youtube/v3/search" + q;
+	//establish variables to build AJAX call
 
-	$("#results-items").on("click", ".detail", function () {
+	$("#results-items, #my-favs").on("click", ".detail", function () {
 		q = $(this).data('track-name');
 		console.log(q);
 		$("#vidTitle").html(q);
@@ -16,18 +18,19 @@ $(document).ready(function(){
 		q = $(this).data('track-name');
 		$("#vidTitle").html(q);
 
-  $.ajax({
-    method: "GET",
-    url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q='+ q +'&regionCode=us&type=video&videoCategoryId=10&videoDefinition=any&videoEmbeddable=true&videoSyndicated=true&key=AIzaSyCsoCs7rQgLp_KQrlZP8oe6UHvo6taoHbY'
-  })
-    .done(function(response){
-      
-     var results = response.items;
-     var youtubeSearch = 'https://www.youtube.com/embed/' + results[0].id.videoId;
-     
-     $("#vidHere").attr("src", youtubeSearch + "?autoplay=1");
-    });
-    
-  });
+		$.ajax({
+				method: "GET",
+				url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=' + q + '&regionCode=us&type=video&videoCategoryId=10&videoDefinition=any&videoEmbeddable=true&videoSyndicated=true&key=AIzaSyCsoCs7rQgLp_KQrlZP8oe6UHvo6taoHbY'
+			})
+			.done(function (response) {
+
+				var results = response.items;
+				var youtubeSearch = 'https://www.youtube.com/embed/' + results[0].id.videoId;
+
+				$("#vidHere").attr("src", youtubeSearch + "?autoplay=1");
+			});
+	});
+	
+
 
 });
