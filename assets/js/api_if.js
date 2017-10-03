c3131eed77c28c94cfd24c7ee783214d59ef8ea4
Alan Davison <abdavison@cox.net>
2017-10-03 16:58:14 -0700

    Clean up. Remove unused stand-alone functions. Engineer api Object to use internal query builder functions.

--- assets/js/api_if.js
+++ assets/js/api_if.js
@@ -22,9 +22,9 @@ var apiObj = {
 		searchParam = (typeof searchParam !== 'undefined') ?  searchParam : 'page_size=10&page=1&s_track_rating=desc&';	// default to this if no param is given\
 
 		var search = searchString;
-		var baseUrl = getBaseUrl(apiName);
-		var searchType = getSearchType('keywordSearch');
-		var key = getKey(apiName);
+		var baseUrl = this.getBaseUrl(apiName);
+		var searchType = this.getSearchType('keywordSearch');
+		var key = this.getKey(apiName);
 		queryString = baseUrl + searchType + 'q=' + searchString + '&' + searchParam + key;
 		//console.log("apiObj.keywordSearch: queryString: ", queryString);
 
@@ -37,9 +37,9 @@ var apiObj = {
 		searchParam = (typeof searchParam !== 'undefined') ?  searchParam : '';	// default to this if no param is given\
 
 		var search = searchString;
-		var baseUrl = getBaseUrl(apiName);
-		var searchType = getSearchType('songSearch');
-		var key = getKey(apiName);
+		var baseUrl = this.getBaseUrl(apiName);
+		var searchType = this.getSearchType('songSearch');
+		var key = this.getKey(apiName);
 		queryString = baseUrl + searchType + 'track_id=' + searchString + '&' + searchParam + key;
 		//console.log("apiObj.songSearch: queryString: ", queryString);
 
@@ -47,15 +47,15 @@ var apiObj = {
 	},
 
 	artistSearch: function (searchString, apiName, searchParam) {
-		console.log("apiObj.artistSearch fn: searchString: ", searchString);
+		//console.log("apiObj.artistSearch fn: searchString: ", searchString);
 		apiName = (typeof apiName !== 'undefined') ?  apiName : 'musixmatch';		// default to musixMatch if no name is given
 		searchParam = (typeof searchParam !== 'undefined') ?  searchParam : 'page_size=10&page=1&s_track_rating=desc&';	// default to this if no param is given\
 
 		var search = searchString;
-		var baseUrl = getBaseUrl(apiName);
-		var searchType = getSearchType('keywordSearch');
-		var key = getKey(apiName);
-		queryString = baseUrl + searchType + 'q_artist=' + searchString + '&' + searchParam + key;
+		var baseUrl = this.getBaseUrl(apiName);
+		var searchType = this.getSearchType('keywordSearch');
+		var key = this.getKey(apiName);
+		//queryString = baseUrl + searchType + 'q_artist=' + searchString + '&' + searchParam + key;
 		console.log("apiObj.artistSearch: queryString: ", queryString);
 
 		this.callAjax(queryString);
@@ -67,9 +67,9 @@ var apiObj = {
 		searchParam = (typeof searchParam !== 'undefined') ?  searchParam : '';	// default to this if no param is given\
 
 		var search = searchString;
-		var baseUrl = getBaseUrl(apiName);
-		var searchType = getSearchType('lyricsSearch');
-		var key = getKey(apiName);
+		var baseUrl = this.getBaseUrl(apiName);
+		var searchType = this.getSearchType('lyricsSearch');
+		var key = this.getKey(apiName);
 		queryString = baseUrl + searchType + 'track_id=' + searchString + '&' + searchParam + key;
 		console.log("apiObj.lyricsSearch: queryString: ", queryString);
 
@@ -82,31 +82,117 @@ var apiObj = {
 		searchParam = (typeof searchParam !== 'undefined') ?  searchParam : 'page=1&page_size=3&';	// default to this if no param is given\
 
 		var search = searchString;
-		var baseUrl = getBaseUrl(apiName);
-		var searchType = getSearchType('countrySearch');
-		var key = getKey(apiName);
+		var baseUrl = this.getBaseUrl(apiName);
+		var searchType = this.getSearchType('countrySearch');
+		var key = this.getKey(apiName);
 		queryString = baseUrl + searchType + 'country=' + searchString + '&' + searchParam + key;
 		console.log("apiObj.countrySearch: queryString: ", queryString);
 
 		this.callAjax(queryString);
 	},
 
-	videoSearch: function (keyword) {
-		//return apiObj ();
-	},
-
-	audioSearch: function (song) {
-
-	},
-
-	someKindOfSearch: function () {
-
-		return new apiObj (artist_id, artist_name);		// don't need the new...
-	},
-
 	getApiData: function () {
 
 	},
+
+	getBaseUrl: function (apiName) {
+		//console.log("getBaseUrl: apiName", apiName);
+	
+		//var proxy = 'https://cors.io/?'
+		var proxy = 'https://thingproxy.freeboard.io/fetch/'
+		//var proxy = 'https://cors-anywhere.herokuapp.com/'
+
+		var baseUrl = '';
+	
+		switch (apiName) {
+			case 'musixmatch':
+				baseUrl = 'https://api.musixmatch.com/ws/1.1/';
+				break;
+			case 'freemusicarchive':
+				baseUrl = 'https://freemusicarchive.org/api/';
+				break;		
+			case 'youtube':
+				baseUrl = 'https://www.googleapis.com/youtube/v3/';
+				break;		
+			case 'bandsintown':
+				baseUrl = 'https://rest.bandsintown.com/';
+				break;	
+			case 'eventful':
+				baseUrl = 'https://eventful.com/';
+				break;		
+			default:
+				console.log("Error: invalid apiName parameter passed to function getBaseUrl")
+		}	// switch
+	
+	    //console.log("proxy: ", proxy);
+	    //console.log("baseUrl: ", baseUrl);
+	    //console.log("proxy + baseUrl", proxy + baseUrl);
+	
+	    return (proxy + baseUrl);	
+	},	// function getBaseUrl()
+	
+	getKey: function (apiName) {
+		//console.log("getKey: apiName: ", apiName);
+	
+		var thekey = '';
+	
+		switch (apiName) {
+			case 'musixmatch':
+				thekey = 'apikey=4dd81b4d24fc4b88c41b0e8638cc97aa';
+				break;
+			case 'freemusicarchive':
+				thekey = '';
+				break;		
+			case 'youtube':
+				thekey = 'key=AIzaSyAKbYAjNo72FOZ6S0XZoW395R2LTWIm8II';
+				break;		
+			case 'bandsintown':
+				thekey = 'app_id=codingbootcamp';
+				break;		
+			case 'eventful':
+				thekey = 'G8jtQgjTFGVsmvdV';
+				break;		
+			default:
+				console.log("Error: invalid apiName parameter passed to function getKey")
+		}	// switch
+	
+	    //console.log("getKey function: key: ", thekey);
+	
+	    return (thekey);	
+	}, // getKey()
+	
+	getSearchType: function (searchType, secondParam) {
+		//console.log("getSearchType function searchType: ", searchType);
+		//console.log("getSearchType function secondParam: ", secondParam);
+	
+		var searchType = searchType; // does it work without this line?
+	
+		switch (searchType) {
+			case 'countrySearch': 			// musixMatch only for now - chart.artists.get?page=1&page_size=3&country=it& 'apikey=4dd81b4d24fc4b88c41b0e8638cc97aa'
+				searchTypeString = 'chart.artists.get?';
+				break;
+			case 'songSearch': 			// fma - trackSearch?q=deerhoof&limit=10' 
+				searchTypeString = 'track.get?';
+				break;		
+			case 'videoSearch': 			// youTube part=snippet&order=rating&type=video&videoDefinition=high&videoEmbeddable=true& 'key=AIzaSyAKbYAjNo72FOZ6S0XZoW395R2LTWIm8II'
+				searchTypeString = '';
+				break;		
+			case 'lyricsSearch': 						// bandsInTown artists/van morrison?', 'app_id=codingbootcamp'
+				searchTypeString = 'track.lyrics.get?';
+				break;		
+			case 'keywordSearch': 						// musixmatch track.search?q=sun&page_size=10&page=1&s_track_rating=desc&', 'apikey=4dd81b4d24fc4b88c41b0e8638cc97aa'
+				searchTypeString = 'track.search?';	// track.search?q=sun&page_size=10&page=1&s_track_rating=desc&', 'apikey=4dd81b4d24fc4b88c41b0e8638cc97aa'';
+				break;		
+			case 'test': 						// musixmatch
+				searchTypeString = '';
+				break;		
+			default:
+				console.log("Error: invalid seachType parameter passed to function getsearchType")
+		}	// switch
+	
+	    //console.log("searchTypeString: ", searchTypeString);
+	    return (searchTypeString);	
+	},	// function getsearchType()
 
 	callAjax: function (queryString) {
   		//console.log("apiObj.callAjax function");
@@ -142,7 +228,7 @@ var apiObj = {
 	
 	processResponse: function (response) {
 
-		console.log ("apiObj: processResponse: response", response);
+		//console.log ("apiObj: processResponse: response", response);
 
 		if (typeof response.message.body != undefined) {
 			console.log("apiObj: processResponse: response.message.body", response.message.body);
@@ -155,7 +241,7 @@ var apiObj = {
 				this.tracks.push( this.buildTrack(response.message.body.track_list[i]) );
 			}
 
-			console.log('TRACKS: ', this.tracks);
+			//console.log('TRACKS: ', this.tracks);
 			this.showTracks(this.tracks, "#results-items");
 		};
 
@@ -163,7 +249,7 @@ var apiObj = {
 
 			this.track = this.buildTrack(response.message.body);
 
-			console.log('TRACK: ', this.track);
+			//console.log('TRACK: ', this.track);
 			this.showTrack(this.track, "#my-favs");
 		};
 
@@ -179,12 +265,13 @@ var apiObj = {
 
 			this.lyrics_body = response.message.body.lyrics.lyrics_body;
 
-			this.showTracks();
+			//console.log("LYRICS: ", this.lyrics_body);
+			this.showTrack(this.track, "#my-favs");
 
 		};
 
 	},
-	
+
 	populateTrackDetail: function(track){
 			$("#detail-track").html(track.track_name);
 			$("#detail-name").html(track.artist_name);
@@ -220,208 +307,9 @@ var apiObj = {
 
 };	// apiObject
 
-/**
- * Plain Vanilla Functions - you can call these on their own to return a result string 
- * 	- they do not affect the apiObj{}
- * 	- return = string result(s)
- */
-
-function keywordSearch (searchString, apiName, searchParam) {
-	apiName = (typeof apiName !== 'undefined') ?  apiName : 'musixmatch';						// default to musixMatch if no name is given
-	searchParam = (typeof searchParam !== 'undefined') ?  searchParam : 'page_size=10&page=1&s_track_rating=desc&';	// default to this if no param is given\
-
-	var search = searchString;
-	var baseUrl = getBaseUrl(apiName);
-	var searchType = getSearchType('keywordSearch');
-	var key = getKey(apiName);
-	queryString = baseUrl + searchType + 'q=' + searchString + '&' + searchParam + key;
-	console.log("keywordSearch: queryString: ", queryString);
-
-	callAjax(queryString);
-}
-
-function songSearch (searchString, apiName, searchParam) {
-	apiName = (typeof apiName !== 'undefined') ?  apiName : 'musixmatch';						// default to musixMatch if no name is given
-	searchParam = (typeof searchParam !== 'undefined') ?  searchParam : 'page_size=10&page=1&s_track_rating=desc&';	// default to this if no param is given\
-
-	var search = searchString;
-	var baseUrl = getBaseUrl(apiName);
-	var searchType = getSearchType('keywordSearch');
-	var key = getKey(apiName);
-	queryString = baseUrl + searchType + 'q_track=' + searchString + '&' + searchParam + key;
-	console.log("songSearch: queryString: ", queryString);
-
-	callAjax(queryString);
-}
-
-function artistSearch (searchString, apiName, searchParam) {
-	apiName = (typeof apiName !== 'undefined') ?  apiName : 'musixmatch';						// default to musixMatch if no name is given
-	searchParam = (typeof searchParam !== 'undefined') ?  searchParam : 'page_size=10&page=1&s_track_rating=desc&';	// default to this if no param is given\
-
-	var search = searchString;
-	var baseUrl = getBaseUrl(apiName);
-	var searchType = getSearchType('keywordSearch');
-	var key = getKey(apiName);
-	queryString = baseUrl + searchType + 'q_artist=' + searchString + '&' + searchParam + key;
-	console.log("artistSearch: queryString: ", queryString);
-
-	callAjax(queryString);
-}
-
-function lyricsSearch (searchString, apiName, searchParam) {
-	apiName = (typeof apiName !== 'undefined') ?  apiName : 'musixmatch';						// default to musixMatch if no name is given
-	searchParam = (typeof searchParam !== 'undefined') ?  searchParam : '';	// default to this if no param is given\
-
-	var search = searchString;
-	var baseUrl = getBaseUrl(apiName);
-	var searchType = getSearchType('lyricsSearch');
-	var key = getKey(apiName);
-	queryString = baseUrl + searchType + 'q_artist=' + searchString + '&' + searchParam + key;
-	console.log("artistSearch: queryString: ", queryString);
-
-	callAjax(queryString);
-}
-
-function countrySearch (searchString, apiName, searchParam) {
-		console.log("apiObj.countrySearch fn: searchString: ", searchString);
-		apiName = (typeof apiName !== 'undefined') ?  apiName : 'musixmatch';		// default to musixMatch if no name is given
-		searchParam = (typeof searchParam !== 'undefined') ?  searchParam : 'page=1&page_size=3&';	// default to this if no param is given\
-
-		var search = searchString;
-		var baseUrl = getBaseUrl(apiName);
-		var searchType = getSearchType('countrySearch');
-		var key = getKey(apiName);
-		queryString = baseUrl + searchType + 'track_id=' + searchString + '&' + searchParam + key;
-		console.log("apiObj.lyricsSearch: queryString: ", queryString);
-
-		this.callAjax(queryString);
-}
 
 
-/**
- * Helper Functions
- *  - used by other functions
- *  - return a string to use, such as baseUrl e.g.
- *  - or return a response object
- *  - or populate the apiObj {}
- */
 
-function getBaseUrl (apiName) {
-	//console.log("getBaseUrl: apiName", apiName);
-
-	//var proxy = 'https://cors.io/?'
-	//var proxy = 'http://cors-proxy.htmldriven.com/?url='
-	//var proxy = 'https://crossorigin.me/'
-	var proxy = 'https://cors-anywhere.herokuapp.com/'
-	var baseUrl = '';
-
-	switch (apiName) {
-		case 'musixmatch':
-			baseUrl = 'https://api.musixmatch.com/ws/1.1/';
-			break;
-		case 'freemusicarchive':
-			baseUrl = 'https://freemusicarchive.org/api/';
-			break;		
-		case 'youtube':
-			baseUrl = 'https://www.googleapis.com/youtube/v3/';
-			break;		
-		case 'bandsintown':
-			baseUrl = 'https://rest.bandsintown.com/';
-			break;	
-		case 'eventful':
-			baseUrl = 'https://eventful.com/';
-			break;		
-		default:
-			console.log("Error: invalid apiName parameter passed to function getBaseUrl")
-	}	// switch
-
-    //console.log("proxy: ", proxy);
-    //console.log("baseUrl: ", baseUrl);
-    //console.log("proxy + baseUrl", proxy + baseUrl);
-
-    return (proxy + baseUrl);	
-}	// function getBaseUrl()
-
-function getKey(apiName) {
-	//console.log("getKey: apiName: ", apiName);
-
-	var thekey = '';
-
-	switch (apiName) {
-		case 'musixmatch':
-			thekey = 'apikey=4dd81b4d24fc4b88c41b0e8638cc97aa';
-			break;
-		case 'freemusicarchive':
-			thekey = '';
-			break;		
-		case 'youtube':
-			thekey = 'key=AIzaSyAKbYAjNo72FOZ6S0XZoW395R2LTWIm8II';
-			break;		
-		case 'bandsintown':
-			thekey = 'app_id=codingbootcamp';
-			break;		
-		case 'eventful':
-			thekey = 'G8jtQgjTFGVsmvdV';
-			break;		
-		default:
-			console.log("Error: invalid apiName parameter passed to function getKey")
-	}	// switch
-
-    //console.log("getKey function: key: ", thekey);
-
-    return (thekey);	
-} // getKey()
-
-function getSearchType (searchType, secondParam) {
-	//console.log("getSearchType function searchType: ", searchType);
-	//console.log("getSearchType function secondParam: ", secondParam);
-
-	var searchType = searchType; // does it work without this line?
-
-	switch (searchType) {
-		case 'countrySearch': 			// musixMatch only for now - chart.artists.get?page=1&page_size=3&country=it& 'apikey=4dd81b4d24fc4b88c41b0e8638cc97aa'
-			searchTypeString = 'chart.artists.get?';
-			break;
-		case 'songSearch': 			// fma - trackSearch?q=deerhoof&limit=10' 
-			searchTypeString = 'track.get?';
-			break;		
-		case 'videoSearch': 			// youTube part=snippet&order=rating&type=video&videoDefinition=high&videoEmbeddable=true& 'key=AIzaSyAKbYAjNo72FOZ6S0XZoW395R2LTWIm8II'
-			searchTypeString = '';
-			break;		
-		case 'lyricsSearch': 						// bandsInTown artists/van morrison?', 'app_id=codingbootcamp'
-			searchTypeString = 'track.lyrics.get?';
-			break;		
-		case 'keywordSearch': 						// musixmatch track.search?q=sun&page_size=10&page=1&s_track_rating=desc&', 'apikey=4dd81b4d24fc4b88c41b0e8638cc97aa'
-			searchTypeString = 'track.search?';	// track.search?q=sun&page_size=10&page=1&s_track_rating=desc&', 'apikey=4dd81b4d24fc4b88c41b0e8638cc97aa'';
-			break;		
-		case 'test': 						// musixmatch
-			searchTypeString = '';
-			break;		
-		default:
-			console.log("Error: invalid seachType parameter passed to function getsearchType")
-	}	// switch
-
-    //console.log("searchTypeString: ", searchTypeString);
-    return (searchTypeString);	
-}	// function getsearchType()
-
-function callAjax (queryString) {
-  	//console.log("callAjax function");
-
-	var queryString = queryString;
-
-    //console.log("callAjax function: queryString: ", queryString);
-
-	$.ajax({
-		  dataType: 'json',		// otherwise you get a very long char array!
-          url: queryString,
-          method: "GET"    
-        }).done(function(response) {
-            //console.log("callAjax: response: ", JSON.parse(response));
-            gblResponse = response;								// any reason we can't do this?
-    });
-
-}
 
 
 // FOR DEBUG ONLY
